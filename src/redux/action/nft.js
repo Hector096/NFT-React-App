fc6c961fe2b2b969599b61a5e9efb659be4f4abc
Vishal Verma <vishal@genetico.in>
2022-05-15 19:28:39 +0530

    add actions

--- src/redux/action/nft.js
+++ src/redux/action/nft.js
@@ -1,112 +1,149 @@
-// /* eslint-disable no-console */
-// import {
-//   NFT_FETCH_SUCCESS,
-//   NFT_UPDATE_FAIL,
-//   NFT_FETCH_FAIL,
-//   SET_MESSAGE,
-//   NFT_UPDATE_SUCCESS,
-//   NFT_DELETE_FAIL,
-//   NFT_DELETE_SUCCESS,
-// } from './types';
+/* eslint-disable no-console */
+import {
+  NFT_FETCH_SUCCESS,
+  NFT_UPDATE_FAIL,
+  NFT_FETCH_FAIL,
+  NFT_ADD_SUCCESS,
+  NFT_ADD_FAIL,
+  SET_MESSAGE,
+  NFT_UPDATE_SUCCESS,
+  NFT_DELETE_FAIL,
+  NFT_DELETE_SUCCESS,
+} from './types';
 
-// import UserService from '../../service/user.service';
+import NftService from '../../service/nft.service';
 
-// // eslint-disable-next-line max-len
-// export const addProduct = (values) => (dispatch) => UserService.addProducts(values).then(
-//   (response) => {
-//     console.log(response.data);
-//     dispatch({
-//       type: PRODUCT_SUCCESS,
-//       payload: response.data.product,
-//     });
+// eslint-disable-next-line max-len
+export const addNft = (values) => (dispatch) => NftService.addNft(values).then(
+  (response) => {
+    console.log(response.data);
+    dispatch({
+      type: NFT_ADD_SUCCESS,
+      payload: response.data.product,
+    });
 
-//     dispatch({
-//       type: SET_MESSAGE,
-//       payload: response.data.message,
-//     });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: response.data.message,
+    });
 
-//     return Promise.resolve();
-//   },
-//   (error) => {
-//     console.log(error.response);
-//     const message = (error.response
-//                 && error.response.data
-//                 && error.response.data.message)
-//               || error.message
-//               || error.toString();
-//     dispatch({
-//       type: PRODUCT_FAIL,
-//     });
+    return Promise.resolve();
+  },
+  (error) => {
+    console.log(error.response);
+    const message = (error.response
+                && error.response.data
+                && error.response.data.message)
+              || error.message
+              || error.toString();
+    dispatch({
+      type: NFT_ADD_FAIL,
+    });
 
-//     dispatch({
-//       type: SET_MESSAGE,
-//       payload: message,
-//     });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
 
-//     return Promise.reject();
-//   },
-// );
+    return Promise.reject();
+  },
+);
 
-// export const fetchProducts = () => (dispatch) => UserService.fetchProducts().then(
-//   (response) => {
-//     dispatch({
-//       type: PRODUCT_FETCH_SUCCESS,
-//       payload: { products: response.data.products },
-//     });
+// eslint-disable-next-line max-len
+export const updateNft = (values, id) => (dispatch) => NftService.updateNft(values, id).then(
+  (response) => {
+    dispatch({
+      type: NFT_UPDATE_SUCCESS,
+      payload: response.data,
+    });
 
-//     dispatch({
-//       type: SET_MESSAGE,
-//       payload: response.data.message,
-//     });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: response.data.message,
+    });
 
-//     return Promise.resolve();
-//   },
-//   (error) => {
-//     console.log(error.response);
-//     const message = (error.response
-//                 && error.response.data
-//                 && error.response.data.message)
-//               || error.message
-//               || error.toString();
-//     dispatch({
-//       type: PRODUCT_FETCH_FAIL,
-//     });
+    return Promise.resolve();
+  },
+  (error) => {
+    console.log(error.response);
+    const message = (error.response
+                  && error.response.data
+                  && error.response.data.message)
+                || error.message
+                || error.toString();
+    dispatch({
+      type: NFT_UPDATE_FAIL,
+    });
 
-//     dispatch({
-//       type: SET_MESSAGE,
-//       payload: message,
-//     });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
 
-//     return Promise.reject();
-//   },
-// );
+    return Promise.reject();
+  },
+);
 
-// export const deleteProduct = (id) => (dispatch) => UserService.deleteProduct(id).then(
-//   // eslint-disable-next-line
-//     (response) => {
-//     dispatch({
-//       type: PRODUCT_DELETE_SUCCESS,
-//       payload: id,
-//     });
+export const fetchNfts = () => (dispatch) => NftService.fetchNfts().then(
+  (response) => {
+    dispatch({
+      type: NFT_FETCH_SUCCESS,
+      payload: { products: response.data.products },
+    });
 
-//     return Promise.resolve();
-//   },
-//   (error) => {
-//     console.log(error.response);
-//     const message = (error.response
-//                 && error.response.data
-//                 && error.response.data.message)
-//               || error.message
-//               || error.toString();
-//     dispatch({
-//       type: PRODUCT_DELETE_FAIL,
-//     });
+    dispatch({
+      type: SET_MESSAGE,
+      payload: response.data.message,
+    });
 
-//     dispatch({
-//       type: SET_MESSAGE,
-//       payload: message,
-//     });
+    return Promise.resolve();
+  },
+  (error) => {
+    console.log(error.response);
+    const message = (error.response
+                && error.response.data
+                && error.response.data.message)
+              || error.message
+              || error.toString();
+    dispatch({
+      type: NFT_FETCH_FAIL,
+    });
 
-//     return Promise.reject();
-//   },
-// );
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
+
+    return Promise.reject();
+  },
+);
+
+export const deleteNft = (id) => (dispatch) => NftService.deleteNft(id).then(
+  // eslint-disable-next-line
+    (response) => {
+    dispatch({
+      type: NFT_DELETE_SUCCESS,
+      payload: id,
+    });
+
+    return Promise.resolve();
+  },
+  (error) => {
+    console.log(error.response);
+    const message = (error.response
+                && error.response.data
+                && error.response.data.message)
+              || error.message
+              || error.toString();
+    dispatch({
+      type: NFT_DELETE_FAIL,
+    });
+
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
+
+    return Promise.reject();
+  },
+);
--- src/service/nft.service.js
+++ src/service/nft.service.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const API_URL = 'https://stark-badlands-18948.herokuapp.com';
 
-// eslint-disable-next-line
 const deleteNft = (id) => axios.delete(`${API_URL}/nft/${id}`);
 
-// eslint-disable-next-line
-const addNft = (values) => axios.post(`${API_URL}/nft`,values);
+const addNft = (values) => axios.post(`${API_URL}/nft`, values);
 
-// eslint-disable-next-line
+const updateNft = (values, id) => axios.post(`${API_URL}/nft/${id}`, values);
+
 const fetchNfts = () => axios.get(`${API_URL}/nft`);
 
 export default {
   fetchNfts,
   addNft,
   deleteNft,
+  updateNft,
 };
